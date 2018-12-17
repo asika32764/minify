6bb3cdcaaa1b38f27e2acc934d6f9c9ee335062f
Umut Işık <umutphp@gmail.com>
2018-12-17 22:14:45 +0300

    Fix the bug #1 on Github
    A non well formed numeric value encountered

--- src/CSS/CSSmin.php
+++ src/CSS/CSSmin.php
@@ -784,9 +784,9 @@ class CSSmin
 	{
 		if (is_string($size)) {
 			switch (substr($size, -1)) {
-				case 'M': case 'm': return $size * 1048576;
-				case 'K': case 'k': return $size * 1024;
-				case 'G': case 'g': return $size * 1073741824;
+				case 'M': case 'm': return intval($size) * 1048576;
+				case 'K': case 'k': return intval($size) * 1024;
+				case 'G': case 'g': return intval($size) * 1073741824;
 			}
 		}
 
